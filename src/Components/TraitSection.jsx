1a86a0bc1897966840f3585ed477857916824ed2
Kelvide <68742854+Kelvide@users.noreply.github.com>
2022-08-16 16:28:11 +0100

    Update TraitSection.jsx

--- src/Components/TraitSection.jsx
+++ src/Components/TraitSection.jsx
@@ -4,7 +4,7 @@ const TraitSection = () => {
     const traitSection = useSelector(state=>state.traitSection)
   return (
     <div className="lg:each_section py-10 md:p-10 lg:px-0 lg:py-16 trait relative bg-cyberLightPurple h-fit">
-        <video src="/assets/dna.mp4" autoPlay loop muted playsInline preload="true" data-wf-ignore="true" className="absolute left-0 top-0 h-full lg:h-auto lg:w-full object-cover w-auto vid_dna">
+        <video src="/assets/dna.mp4" autoPlay loop muted playsInline data-wf-ignore="true" className="absolute left-0 top-0 h-full lg:h-auto lg:w-full object-cover w-auto vid_dna">
         </video>
         <div className="contentmax_width h-5/6 my-auto flex flex-col items-center justify-between px-10">
             <h2 className="top_title">Traits</h2>
