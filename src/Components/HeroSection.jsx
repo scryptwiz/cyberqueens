14833ff0fa2a5647271c65ff5db9f72dd4af528c
Kelvide <68742854+Kelvide@users.noreply.github.com>
2022-08-16 15:46:06 +0100

    Update HeroSection.jsx

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -27,7 +27,7 @@ const HeroSection = () => {
                             <img src={items.fields.image.fields.file.url} className='w-full h-fit object-contain' alt={items.fields.image.fields.title} />
                         </div>
                     </div>
-                    <div key={index} style={{backgroundImage:`linear-gradient(rgba(141, 83, 141,0.5), rgba(141, 83, 141,0.5)), url(${bgImage})`}} className="relative heroImg flex contentmax_width h-5/6 my-auto lg:hidden items-center justify-between"> 
+                    <div key={index} style={{backgroundImage:`linear-gradient(rgba(141, 83, 141,0.5), rgba(141, 83, 141,0.5)), url(${bgImage})`}} className="bg-no-repeat relative heroImg flex contentmax_width h-5/6 my-auto lg:hidden items-center justify-between"> 
                         <div className="flex flex-col items-start gap-5 w-full lg:w-1/2 px-7 opacity-100 z-10">
                             <h1 className="text-5xl md:text-5xl text-white">{items.fields.title}</h1>
                             <h1 className="text-sm text-white">{items.fields.description}</h1>
